039c91217963f3ff05b7f1d8d53c3a8b2e3c023c
rui2015 <francisco21maio@gmail.com>
2021-01-03 00:18:21 +0000

    Change required permissions

--- Commands/invite.js
+++ Commands/invite.js
@@ -7,5 +7,5 @@ module.exports = {
 };
 
 function runcommand(client, command, message, args){
-    message.reply('here is the Invite Link:\n<https://discord.com/api/oauth2/authorize?client_id=766040022414786560&permissions=0&scope=bot>');
+    message.reply('here is the Invite Link:\n<https://discord.com/api/oauth2/authorize?client_id=766040022414786560&permissions=150528&scope=bot>');
 }
